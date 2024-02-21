c315bb249718237a7a5296f3da0e8c930e345445
eun1230 <jsily2288@naver.com>
2024-02-21 17:00:00 +0900

    javascript

---
+++ 06_javascript/01_start.js
@@ -0,0 +1,2 @@
+alert("링크방식 (head > script의 src속성 > 파일경로입력 ) alert");
+console.log("링크방식 (head > script의 src속성 > 파일경로입력 ) console");
---
+++ 06_javascript/prc.js
@@ -0,0 +1,4 @@
+let mathScore = 77;
+let engScore = 88;
+let avgScore = (77 + 88) / 2;
+console.log(avgScore);
