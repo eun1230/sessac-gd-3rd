649691511eb3d4412eef9d383a96273691c59a2d
eun1230 <jsily2288@naver.com>
2024-02-23 15:52:58 +0900

    typeof

---
+++ 06_javascript/05_typeof.js
@@ -0,0 +1,24 @@
+/*
+typeof 자료형 확인
+------------------------------------------------------------------------------------------
+해당 자료형이 어떤것인지 확인 가능
+------------------------------------------------------------------------------------------
+*/
+
+console.log(typeof "문자열"); //string
+console.log(typeof 234); //number
+console.log(typeof {}); //object
+console.log(typeof []); //object
+console.log(typeof NaN); // Nan : Not a Number -> number
+let und;
+console.log(typeof und); // undefined
+
+
+
+
+
+
+
+
+
+
